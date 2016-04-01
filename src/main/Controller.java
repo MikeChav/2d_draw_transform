4210bf8f1327e59d97d7b569b6028da42eb570cf
MikeChav <mcchavrimootoo@yahoo.com>
2016-04-01 12:24:14 +0400

    Combined main and canvas

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -2,8 +2,41 @@ package main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller  implements Initializable {
+
+	public Canvas myCanvas;
+	public VBox myBox;
+
+	GraphicsContext graphicsContext;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		graphicsContext = myCanvas.getGraphicsContext2D();
+
+//		myBox.prefWidthProperty().bind(myCanvas.widthProperty());
+//		myBox.prefHeightProperty().bind(myCanvas.heightProperty());
+
+		myCanvas.widthProperty().bind(myBox.minWidthProperty());
+		myCanvas.heightProperty().bind(myBox.minHeightProperty());
+
+		graphicsContext.setFill(Color.RED);
+		graphicsContext.fillRect(0, 0, myCanvas.getWidth(), myCanvas.getHeight());
+
+		graphicsContext.setStroke(Color.BLACK);
+		graphicsContext.setLineWidth(2.0);
+		graphicsContext.strokeLine(200.0, 500.0, 200.0, 500.0);
+
+
+	}
 
 	@FXML
 	private void exitAll(ActionEvent actionEvent) throws Exception{
