ac48dffa8c2af0a17ffe3194b02f348929299bee
MikeChav <mcchavrimootoo@yahoo.com>
2016-04-02 01:02:05 +0400

    It's lighter, have fun

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -38,14 +38,14 @@ public class Controller  implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 
         Axes axes = new Axes(
-                1200, 700,
+                1276, 700,
                 -8, 8, 1,
                 -6, 6,1
         );
 
 
         StackPane layout = new StackPane(axes);
-        layout.setStyle("-fx-background-color: forestgreen");
+        layout.setStyle("-fx-background-color: slategrey");
         Canvas v  = (Canvas)Container.getChildren().get(0);
         Container.getChildren().clear();
         Container.getChildren().addAll(layout,v);
@@ -79,16 +79,6 @@ public class Controller  implements Initializable {
 			}
 			listOfPoints.add(mouseLocation);
 		});
-
-
-
-//
-//
-//		addListeners();
-
-//
-
-
 	}
 
 	private void bresenhamLine( double x0, double y0, double x1, double y1) {
@@ -116,117 +106,5 @@ public class Controller  implements Initializable {
 	public  void exitAll(){
 		staticExitAll();
 	}
-
-//	class Axes extends Pane {
-//		private NumberAxis xAxis;
-//		private NumberAxis yAxis;
-//
-//		public Axes(
-//				int width, int height,
-//				double xLow, double xHi, double xTickUnit,
-//				double yLow, double yHi, double yTickUnit
-//		) {
-//			setMinSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
-//			setPrefSize(width, height);
-//			setMaxSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
-//
-//			xAxis = new NumberAxis(xLow, xHi, xTickUnit);
-//			xAxis.setSide(Side.BOTTOM);
-//			xAxis.setMinorTickVisible(false);
-//			xAxis.setPrefWidth(width);
-//			xAxis.setLayoutY(height / 2);
-//
-//			yAxis = new NumberAxis(yLow, yHi, yTickUnit);
-//			yAxis.setSide(Side.LEFT);
-//			yAxis.setMinorTickVisible(false);
-//			yAxis.setPrefHeight(height);
-//			yAxis.layoutXProperty().bind(
-//					Bindings.subtract(
-//							(width / 2) + 1,
-//							yAxis.widthProperty()
-//					)
-//			);
-//
-//			getChildren().setAll(xAxis, yAxis);
-//		}
-//
-//		public NumberAxis getXAxis() {
-//			return xAxis;
-//		}
-//
-//		public NumberAxis getYAxis() {
-//			return yAxis;
-//		}
-//	}
-//
-
-//
-//	class Plot extends Pane {
-//		public Plot(
-//				Function<Double, Double> f,
-//				double xMin, double xMax, double xInc,
-//				Axes axes
-//		) {
-//			Path path = new Path();
-//			path.setStroke(Color.ORANGE.deriveColor(0, 1, 1, 0.6));
-//			path.setStrokeWidth(2);
-//
-//			path.setClip(
-//					new Rectangle(
-//							0, 0,
-//							axes.getPrefWidth(),
-//							axes.getPrefHeight()
-//					)
-//			);
-//
-//			double x = xMin;
-//			double y = f.apply(x);
-//
-//			path.getElements().add(
-//					new MoveTo(
-//							mapX(x, axes), mapY(y, axes)
-//					)
-//			);
-//
-//			x += xInc;
-//			while (x < xMax) {
-//				y = f.apply(x);
-//
-//				path.getElements().add(
-//						new LineTo(
-//								mapX(x, axes), mapY(y, axes)
-//						)
-//				);
-//
-//				x += xInc;
-//			}
-//
-//			setMinSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
-//			setPrefSize(axes.getPrefWidth(), axes.getPrefHeight());
-//			setMaxSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
-//
-//			getChildren().setAll(axes, path);
-//		}
-//
-//		private double mapX(double x, Axes axes) {
-//			double tx = axes.getPrefWidth() / 2;
-//			double sx = axes.getPrefWidth() /
-//					(axes.getXAxis().getUpperBound() -
-//							axes.getXAxis().getLowerBound());
-//
-//			return x * sx + tx;
-//		}
-//
-//		private double mapY(double y, Axes axes) {
-//			double ty = axes.getPrefHeight() / 2;
-//			double sy = axes.getPrefHeight() /
-//					(axes.getYAxis().getUpperBound() -
-//							axes.getYAxis().getLowerBound());
-//
-//			return -y * sy + ty;
-//		}
-//
-//	}
-
 
 }
