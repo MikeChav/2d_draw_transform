f39243bb6f171535dcdde514a67dd40da520341c
MikeChav <mcchavrimootoo@yahoo.com>
2016-04-01 20:00:03 +0400

    Alert

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,10 +1,13 @@
 package main;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
@@ -39,11 +42,15 @@ public class Controller  implements Initializable {
 	}
 
 	@FXML
-	private void exitAll(ActionEvent actionEvent) throws Exception{
-/*
-		Implemented for testing purposes
-*/
-		(new AlertBox()).confirmExit();
+	public static void staticExitAll() {
+		Alert myAlert = new Alert(Alert.AlertType.CONFIRMATION ,"Do you really wanna exit?");
+
+		myAlert.showAndWait()
+				.filter(response -> response == ButtonType.OK)
+				.ifPresent(response -> Platform.exit());
+	}
+	public void exitAll() {
+		staticExitAll();
 	}
 
 }
